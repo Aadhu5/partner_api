2343e8ec18b11f995a76c4e186f218b3ca41a020
Aadhu5 <lavanya.sitecamel@gmail.com>
2021-09-11 15:41:47 +0530

    partner api

--- connection.js
+++ connection.js
@@ -1,12 +1,20 @@
 const mysql = require("mysql");
-
-var mysqlConnection = mysql.createConnection({
+/*var mysqlConnection = mysql.createConnection({
         host : "localhost",
         user : "root",
         password : "",
         database : "partner_5",
         multipleStatements : true
+}); */
+var mysqlConnection = mysql.createConnection({
+        host : "partner-db-do-user-9740444-0.b.db.ondigitalocean.com",
+        user : "doadmin",
+        password : "KAla7QPL-vRVwdtj",
+        database : "defaultdb",
+        multipleStatements : true,
+        sslmode = 'REQUIRED'
 });
+
 mysqlConnection.connect((err)=>{
         if(!err)
         {
